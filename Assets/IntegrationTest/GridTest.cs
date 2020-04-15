2866f6970119f3570ee2d854529bb5a1e1763a5b
Harald Reingruber <harald.reingruber@gmx.at>
2020-04-15 12:03:45 +0200

    WIP: assert that tron is twice as high

--- Assets/IntegrationTest/GridTest.cs
+++ Assets/IntegrationTest/GridTest.cs
@@ -6,15 +6,18 @@ using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
 using Zenject;
 
-namespace IntegrationTest {
-    public class GridTest : SceneTestFixture {
+namespace IntegrationTest
+{
+    public class GridTest : SceneTestFixture
+    {
         // When game starts, there is a tron on the grid.
         // (done) 1. load the main game scene
         // (done) 2. Assert.that("there is a grid") Is it relevant if it is static?
         // 3. Assert.that("there is a tron")
         // 4. Assert.that("tron is at the 'correct' location)
         [UnityTest]
-        public IEnumerator TestGridExists() {
+        public IEnumerator TestGridExists()
+        {
             yield return GivenScene("MainScene");
 
             const string objectId = "Grid";
@@ -56,13 +59,25 @@ namespace IntegrationTest {
         {
             yield return GivenScene("MainScene");
 
-            // yield return new WaitForEndOfFrame();
-
             const string objectId = "Tron";
             var tron = findSingleObjectById(objectId);
             AssertIsVisible(tron, objectId);
-//            assertThatSize(tron, ?);
-//            assertThatPosition(tron, ?);
+            AssertIsTwiceAsHigh(tron, objectId);
+//          assertThatSize(tron, ?);
+//          assertThatPosition(tron, ?);
+        }
+
+        private void AssertIsTwiceAsHigh(GameObject tron, string objectId)
+        {
+            //tron.transform.GetComponent<Tron>()
+
+            var scale = tron.transform.localScale;
+            var width = scale.x;
+            var depth = scale.z;
+            var height = scale.y;
+
+            Assert.That(width, Is.EqualTo(depth));
+            Assert.That(height, Is.EqualTo(width));
         }
 
         /*
