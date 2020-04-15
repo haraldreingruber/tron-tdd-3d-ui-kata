b722646cf92c46f7e41cb7794452b5098d1b23aa
Harald Reingruber <harald.reingruber@gmx.at>
2020-04-15 10:50:14 +0200

    Sketch next test.

--- Assets/IntegrationTest/GridTest.cs
+++ Assets/IntegrationTest/GridTest.cs
@@ -9,8 +9,8 @@ using Zenject;
 namespace IntegrationTest {
     public class GridTest : SceneTestFixture {
         // When game starts, there is a tron on the grid.
-        // 1. load the main game scene
-        // 2. Assert.that("there is a grid") Is it relevant if it is static?
+        // (done) 1. load the main game scene
+        // (done) 2. Assert.that("there is a grid") Is it relevant if it is static?
         // 3. Assert.that("there is a tron")
         // 4. Assert.that("tron is at the 'correct' location)
         [UnityTest]
@@ -23,14 +23,35 @@ namespace IntegrationTest {
 
             var currentSceneName = SceneManager.GetActiveScene().name;
             Assert.That(currentSceneName, Is.EqualTo(sceneName));
+            // TODO extract as givenScene
 
             var grid = Object.FindObjectsOfType<Identifier>()
                 .Where(identifier => identifier.id == "Grid")
                 .Select(identifier => identifier.gameObject).
-                First();
+                First(); // TODO fail if there are more than one
 
             Assert.NotNull(grid, "grid");
             Assert.That(grid.activeInHierarchy, "grid.activeInHierarchy");
+            // open question: Do we need to test the size of the grid?
+            // Definitely not test the colour of the grid.
         }
+
+        /*
+        [UnityTest]
+        public IEnumerator TestTronIsOnGridAtStartingPosition()
+        {
+            yield assertSceneLoaded("MainScene");
+
+            var tron = Object.FindObjectsOfType<Identifier>()
+                .Where(identifier => identifier.id == "Tron")
+                .Select(identifier => identifier.gameObject).
+                First();
+
+            Assert.NotNull(tron, "tron");
+            Assert.That(tron.activeInHierarchy, "tron.activeInHierarchy");
+            assertThatSize(tron, ?);
+            assertThatPosition(tron, ?);
+        }
+        */
     }
 }
