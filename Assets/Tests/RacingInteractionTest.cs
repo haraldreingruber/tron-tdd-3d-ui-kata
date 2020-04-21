b0bcafe48ac6df38e5021eadf336413f8af9944f
Harald Reingruber <harald.reingruber@gmx.at>
2020-04-21 11:52:09 +0200

    unit test Racing StandsStillOnStartup.

--- Assets/Tests/RacingInteractionTest.cs
+++ Assets/Tests/RacingInteractionTest.cs
@@ -23,7 +23,23 @@ namespace Tests
             var newPosition = tronTransform.position;
 
             Assert.That(newPosition.y, Is.EqualTo(originalPosition.y));
-            Assert.That(Math.Abs(newPosition.z - originalPosition.z), Is.GreaterThan(0.0f));
+            Assert.That(Math.Abs(newPosition.z - originalPosition.z), Is.GreaterThan(0.001f));
+        }
+
+        [Test]
+        public void StandsStillOnStartup()
+        {
+            var tron = new GameObject();
+            var racingInteraction = tron.AddComponent<RacingInteraction>();
+
+            var tronTransform = tron.transform;
+            var originalPosition = tronTransform.position;
+
+            racingInteraction.FixedUpdate();
+            // ReSharper disable once Unity.InefficientPropertyAccess - position has changed
+            var newPosition = tronTransform.position;
+
+            Assert.That(newPosition, Is.EqualTo(originalPosition));
         }
 
         /*
