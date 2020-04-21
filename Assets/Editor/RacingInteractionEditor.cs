745fd458fe0afb569d0604177c8cf83709786652
Harald Reingruber <harald.reingruber@gmx.at>
2020-04-21 12:43:57 +0200

    Refactor: move RacingInteractionEditor to Editor folder

---
+++ Assets/Editor/RacingInteractionEditor.cs
@@ -0,0 +1,33 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    [CustomEditor(typeof(RacingInteraction))]
+    public class RacingInteractionEditor : UnityEditor.Editor
+    {
+        // idea taken from: https://docs.unity3d.com/Manual/editor-CustomEditors.html
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            if (GUILayout.Button("Start Racing"))
+            {
+                StartRaceButtonAction();
+            }
+        }
+
+        private void StartRaceButtonAction()
+        {
+            var racingInteraction = (target as RacingInteraction);
+            if (racingInteraction == null)
+            {
+                Debug.Log("No racing interaction assigned");
+                return;
+            }
+
+            racingInteraction.StartRace();
+        }
+    }
+}
