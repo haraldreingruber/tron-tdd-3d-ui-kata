9e306833b99c8ebd78e6d14ee8f056667723bf4f
Harald Reingruber <harald.reingruber@gmx.at>
2020-04-21 12:40:53 +0200

    Feature: trigger startRacing() from Inspector

---
+++ Assets/RacingInteractionEditor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(RacingInteraction))]
+public class RacingInteractionEditor : Editor
+{
+    // idea taken from: https://docs.unity3d.com/Manual/editor-CustomEditors.html
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        if (GUILayout.Button("Start Racing"))
+        {
+            var racingInteraction = (target as RacingInteraction);
+            if (racingInteraction == null)
+            {
+                Debug.Log("No racing interaction assigned");
+                return;
+            }
+
+            racingInteraction.StartRace();
+        }
+    }
+}
