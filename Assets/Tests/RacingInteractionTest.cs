f3426657e06ca8d1b1af941b1fed7ae2b8badd19
Harald Reingruber <harald.reingruber@gmx.at>
2020-04-21 11:46:41 +0200

    unit test Reacing interaction.

---
+++ Assets/Tests/RacingInteractionTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Tests
+{
+    public class RacingInteractionTest
+    {
+        [Test]
+        public void StartsRacing()
+        {
+            // same test, no UI?
+            var tron = new GameObject();
+            var racingInteraction = tron.AddComponent<RacingInteraction>();
+
+            var tronTransform = tron.transform;
+            var originalPosition = tronTransform.position;
+
+            racingInteraction.StartRace();
+
+            racingInteraction.FixedUpdate();
+            // ReSharper disable once Unity.InefficientPropertyAccess - position has changed
+            var newPosition = tronTransform.position;
+
+            Assert.That(newPosition.y, Is.EqualTo(originalPosition.y));
+            Assert.That(Math.Abs(newPosition.z - originalPosition.z), Is.GreaterThan(0.0f));
+        }
+
+        /*
+ * Test list
+ * - do not move unless start racing - UI?
+ * - speed or relative movement - UI?
+ * - leaves walls behind
+ */
+    }
+}
