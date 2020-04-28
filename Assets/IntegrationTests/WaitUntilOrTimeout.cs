ef2934e1c1c8b2013cab36b515b1f680a691d9c7
Harald Reingruber <harald.reingruber@gmx.at>
2020-04-28 18:22:33 +0200

    Cleanup WaitUntilOrTimeout

--- Assets/IntegrationTests/WaitUntilOrTimeout.cs
+++ Assets/IntegrationTests/WaitUntilOrTimeout.cs
@@ -8,18 +8,12 @@ namespace IntegrationTests
         private readonly Func<bool> m_Predicate;
         private readonly float m_Timeout;
 
-        public override bool keepWaiting
-        {
-            get
-            {
-                return Time.time < this.m_Timeout && !this.m_Predicate();
-            }
-        }
+        public override bool keepWaiting => Time.time < m_Timeout && !m_Predicate(); // TODO Do we need to fail if timeout reached?
 
         public WaitUntilOrTimeout(Func<bool> predicate, float timeoutSeconds)
         {
-            this.m_Predicate = predicate;
-            this.m_Timeout = Time.time + timeoutSeconds;
+            m_Predicate = predicate;
+            m_Timeout = Time.time + timeoutSeconds;
         }
     }
 }
