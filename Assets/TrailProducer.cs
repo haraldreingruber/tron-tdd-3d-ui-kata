013d40af24a74db9809e44db21258365c343891f
Harald Reingruber <harald.reingruber@gmx.at>
2020-05-05 12:21:47 +0200

    Trail grows while racing
    Co-Authored-By: Peter Kofler <codecop@users.noreply.github.com>

--- Assets/TrailProducer.cs
+++ Assets/TrailProducer.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using Object = UnityEngine.Object;
 
 public class TrailProducer : MonoBehaviour
 {
@@ -6,19 +8,31 @@ public class TrailProducer : MonoBehaviour
     public GameObject trailsContainer;
 
     private GameObject currentTrail;
+    private Vector3 _parentBackBorder;
 
     public void StartProducing()
     {
-        var parentTransform = this.transform;
-        var parentBachBorder = GetBackBorder(this.gameObject);
+        _parentBackBorder = GetBackBorder(this.gameObject);
 
         currentTrail = Object.Instantiate(trailPrefab, trailsContainer.transform);
         var currentTrailTransform = currentTrail.transform;
-        currentTrailTransform.position = parentBachBorder;
+        currentTrailTransform.position = _parentBackBorder;
         currentTrailTransform.localScale = new Vector3(1, 1, 0.0f);
     }
 
-    private Vector3 GetBackBorder(GameObject obj)
+    private void Update()
+    {
+        if (currentTrail == null)
+            return;
+
+        var currentParentBackBorder = GetBackBorder(this.gameObject);
+        var newLength = (currentParentBackBorder - _parentBackBorder).z;
+        var currentTrailTransform = currentTrail.transform;
+        currentTrailTransform.position = _parentBackBorder + Vector3.forward * (newLength/2.0f);
+        currentTrailTransform.localScale = new Vector3(1, 1, newLength);
+    }
+
+    private Vector3 GetBackBorder(GameObject obj) // TODO: dup with Test
     {
         var transform = obj.transform;
         var position = transform.position;
