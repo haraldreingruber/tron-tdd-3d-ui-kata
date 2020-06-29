1ac0181b0f27e02d6d402185bf25c36ec48ce61e
Harald Reingruber <harald.reingruber@gmx.at>
2020-06-29 12:46:59 +0200

    Fix blinking test with Assert error in onSomething inside framework.
    Co-Authored-By: Peter Kofler <codecop@users.noreply.github.com>

--- Assets/IntegrationTests/KeyboardInputTest.cs
+++ Assets/IntegrationTests/KeyboardInputTest.cs
@@ -11,11 +11,13 @@ namespace IntegrationTests
     {
         // taken from: https://docs.unity3d.com/Packages/com.unity.inputsystem@1.0/manual/Testing.html
         private readonly InputTestFixture _input = new InputTestFixture();
+        private Keyboard _keyboard;
 
         [SetUp]
         public void PrepareInputSystem()
         {
             _input.Setup();
+            _keyboard = InputSystem.AddDevice<Keyboard>();
         }
 
         [TearDown]
@@ -37,8 +39,7 @@ namespace IntegrationTests
             tronTransform.GetComponent<Tron>().StartRace();
             yield return new WaitForEndOfFrame();
 
-            var keyboard = InputSystem.AddDevice<Keyboard>();
-            _input.Press(keyboard.dKey);
+            _input.Press(_keyboard.dKey);
             yield return new WaitForEndOfFrame();
             // yield return new WaitForInputPress(_input).Right();
 
@@ -49,6 +50,7 @@ namespace IntegrationTests
         public IEnumerator DoesntTurnRightWhenGameHasNotStarted()
         {
             yield return Given.Scene(this, "MainScene");
+
             var tron = Find.SingleObjectById("Tron");
             var tronTransform = tron.transform;
             var racingInteraction = tron.AddComponent<RacingInteractionMock>();
@@ -56,8 +58,7 @@ namespace IntegrationTests
             // NO tronTransform.GetComponent<Tron>().StartRace();
             yield return new WaitForEndOfFrame();    
 
-            var keyboard = InputSystem.AddDevice<Keyboard>();
-            _input.Press(keyboard.dKey);
+            _input.Press(_keyboard.dKey);
             yield return new WaitForEndOfFrame();
 
             Assert.That(racingInteraction.TurnRightHasBeenCalled(), Is.False);
