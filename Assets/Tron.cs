bb6392b5d213dffcdcd9df09cdbd4cae121ff852
Harald Reingruber <harald.reingruber@gmx.at>
2020-06-29 11:03:09 +0200

    First working Keyboard Input
    Co-Authored-By: Peter Kofler <codecop@users.noreply.github.com>

--- Assets/Tron.cs
+++ Assets/Tron.cs
@@ -12,14 +12,10 @@ public class Tron : MonoBehaviour
         trailProducer.StartProducing();
     }
     
-    public void OnMove(InputValue value)
+    public void OnMove(InputAction.CallbackContext context)
     {
-        // Read value from control. The type depends on what type of controls.
-        // the action is bound to.
-        var v = value.Get<Vector2>();
-
-        // IMPORTANT: The given InputValue is only valid for the duration of the callback.
-        //            Storing the InputValue references somewhere and calling Get<T>()
-        //            later does not work correctly.
+        var m_Move = context.ReadValue<Vector2>();
+        Debug.Log(m_Move);
     }
+
 }
