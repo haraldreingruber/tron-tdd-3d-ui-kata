5fe8b18d71a40ff0e6803e8be57b68187c24b53b
Harald Reingruber <harald.reingruber@gmx.at>
2020-07-28 11:24:18 +0200

    WIP: Current UI Input experiments didn't improve situation
    Co-Authored-By: Peter Kofler <codecop@users.noreply.github.com>

--- Assets/IntegrationTests/ButtonTest.cs
+++ Assets/IntegrationTests/ButtonTest.cs
@@ -9,10 +9,25 @@ namespace IntegrationTests
 {
     public class ButtonTest : SceneTestFixture
     {
+        private readonly InputPressed _inputPressed = new InputPressed();
+
+        [SetUp]
+        public void Prepare()
+        {
+            _inputPressed.PrepareInputSystem();
+        }
+
+        [TearDown]
+        public void Close()
+        {
+            _inputPressed.CloseInputSystem();
+        }
+
         [UnityTest]
         public IEnumerator StartsGameOnButtonClick() 
         {
             yield return Given.Scene(this, "MainScene");
+            yield return new WaitForEndOfFrame();
 
             var tron = Find.SingleObjectById("Tron");
             var tronSpy = tron.AddComponent<TronSpy>();
@@ -21,9 +36,13 @@ namespace IntegrationTests
             var button = buttonGameObject.GetComponent<Button>();
             button.onClick.RemoveAllListeners();
             button.onClick.AddListener(tronSpy.StartRace);
+            yield return new WaitForEndOfFrame();
+            yield return new WaitForEndOfFrame();
 
             // first try: button.onClick.Invoke(); - works of course
             button.Select();
+            yield return new WaitForSeconds(15);
+            yield return new WaitForEndOfFrame();
             yield return new WaitForEndOfFrame();
 
             Assert.That(tronSpy.StartRacingHasBeenCalled(), Is.True);
