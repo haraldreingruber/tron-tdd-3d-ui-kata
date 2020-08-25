4045cca13d6ea4d9eaecaae984f4dc81e01c4bea
Harald Reingruber <harald.reingruber@gmx.at>
2020-08-25 11:15:52 +0200

    Ignore test which we cannot make work.
    Co-Authored-By: Peter Kofler <codecop@users.noreply.github.com>

--- Assets/IntegrationTests/ButtonTest.cs
+++ Assets/IntegrationTests/ButtonTest.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using NUnit.Framework;
 using UnityEngine;
-using UnityEngine.InputSystem.UI;
 using UnityEngine.TestTools;
 using UnityEngine.UI;
 using Zenject;
@@ -25,37 +24,43 @@ namespace IntegrationTests
         }
 
         [UnityTest]
-        public IEnumerator StartsGameOnButtonClick() 
+        [Ignore("We cannot make it work, skip it after 5 hours trial and error")]
+        public IEnumerator StartsGameOnButtonClick()
         {
             yield return Given.Scene(this, "MainScene");
             yield return new WaitForEndOfFrame();
 
             var tron = Find.SingleObjectById("Tron");
             var tronSpy = tron.AddComponent<TronSpy>();
-            
+
             var buttonGameObject = Find.SingleObjectById("StartButton");
             var button = buttonGameObject.GetComponent<Button>();
             button.onClick.RemoveAllListeners();
             button.onClick.AddListener(tronSpy.StartRace);
+
+            // Trying to  get the simulation of the button click to work. No success.
+            // It feels like Fixture is not connected to UI somehow. Experiments below...
             yield return new WaitForEndOfFrame();
             yield return new WaitForEndOfFrame();
             yield return new WaitForSeconds(10);
-            
+
             // first try: button.onClick.Invoke(); - works of course
-            //button.Select();
+            button.Select();
+
             var buttonRectTransform = button.GetComponent<RectTransform>();
-            var buttonRect = buttonRectTransform.rect;
-            var buttonCenter = buttonRect.position;
+            // var buttonRect = buttonRectTransform.rect;
+            // var buttonCenter = buttonRect.position;
             yield return _inputPressed.MouseLeftClick(buttonRectTransform.position);
-            //
+
             yield return new WaitForSeconds(15);
             yield return new WaitForEndOfFrame();
             yield return new WaitForEndOfFrame();
 
-            Assert.That(tronSpy.StartRacingHasBeenCalled(), Is.True, "tronSpy.StartRacingHasBeenCalled() has been called");
+            Assert.That(tronSpy.StartRacingHasBeenCalled(), Is.True,
+                "tronSpy.StartRacingHasBeenCalled() has been called");
         }
     }
-    
+
     class TronSpy : Tron
     {
         private bool _startRacingHasBeenCalled;
